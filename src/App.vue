c61a86783ec38a8da249f9139c357cdc8e4adad0
Mohammed Ali <alimoh22062000@gmail.com>
2024-04-28 03:46:49 +0100

    [Others], Initial installation and UI design

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+
+  <!-- Header component -->
+  <SiteHeader />
+
+  <div class="container">
+
+    <!-- Balance component -->
+    <TotalBalance />
+
+    <!-- Income Expenses component -->
+    <IncomeExpenses />
+
+    <!-- TransactionList component -->
+    <TransactionList :transactions="state.transactions" />
+    <!-- Sending all our transactions to the TransactionList components as props -->
+
+    <!-- AddTransaction component -->
+    <AddTransaction />
+
+  </div>
+
+</template>
+
+
+<script setup>
+
+// Importing our components
+import SiteHeader from '@/components/SiteHeader.vue';
+import TotalBalance from '@/components/TotalBalance.vue';
+import IncomeExpenses from '@/components/IncomeExpenses.vue';
+import TransactionList from '@/components/TransactionList.vue';
+import AddTransaction from '@/components/AddTransaction.vue';
+
+
+// This is our transaction list. It's now in this 'app.vue', therefore, it'll be global to all the pages.
+import { reactive } from "vue";
+
+const state = reactive({
+  transactions: [
+    { id: 1, text: "Flower", amount: -19.99 },
+    { id: 2, text: "Cars", amount: 299.87 },
+    { id: 3, text: "Tea", amount: -10 },
+    { id: 4, text: "Camera", amount: 150 },
+  ],
+});
+
+</script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h3>History</h3>
+    <ul class="list" id="list">
+      <li
+        v-for="transaction in transactions"
+        :key="transaction.id"
+        :class="{
+          plus: transaction.amount > 0,
+          minus: transaction.amount < 0,
+        }"
+      >
+        {{ transaction.text }}
+        <span>
+          {{ transaction.amount > 0 ? "+" : "-" }}${{ Math.abs(transaction.amount) }}
+        </span>
+        <button class="delete-btn">X</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<style scoped>
+.plus {
+  color: green;
+}
+
+.minus {
+  color: red;
+}
+</style>
+
+<script setup>
+import { defineProps } from "vue";
+
+// Accepting the transactions as props
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
